5f8338c43740b119792796678879733ed3a51761
Sergey Yaskov <yaskovdev@gmail.com>
2024-05-10 23:06:29 +0300

    Cleanup the main model.

--- torch-sandbox/main.py
+++ torch-sandbox/main.py
@@ -24,7 +24,7 @@ def extract_song_snippet(text):
     return songs
 
 
-def load_training_data():
+def load_songs():
     with open(os.path.join(cwd, "data", "irish.abc"), "r") as f:
         text = f.read()
     return extract_song_snippet(text)
@@ -35,17 +35,9 @@ def vectorize_string(string, character_to_index):
 
 
 def get_batch(vectorized_songs, seq_length, batch_size):
-    # the length of the vectorized songs string
-    n = vectorized_songs.shape[0] - 1
-    # randomly choose the starting indices for the examples in the training batch
-    idx = np.random.choice(n - seq_length, batch_size)
-
-    input_batch = [vectorized_songs[i:i + seq_length] for i in idx]
-    output_batch = [vectorized_songs[i + 1:i + seq_length + 1] for i in idx]
-
-    # x_batch, y_batch provide the true inputs and targets for network training
-    x_batch = np.reshape(input_batch, [batch_size, seq_length])
-    y_batch = np.reshape(output_batch, [batch_size, seq_length])
+    random_indexes = np.random.choice(vectorized_songs.shape[0] - seq_length - 1, batch_size)
+    x_batch = np.reshape([vectorized_songs[i:i + seq_length] for i in random_indexes], [batch_size, seq_length])
+    y_batch = np.reshape([vectorized_songs[i + 1:i + seq_length + 1] for i in random_indexes], [batch_size, seq_length])
     return x_batch, y_batch
 
 
@@ -77,72 +69,34 @@ def generate_text(model, char_to_index, index_to_char, start_string, generation_
     return start_string + ''.join(text_generated)
 
 
-if __name__ == '__main__':
-    torch.autograd.set_detect_anomaly(True)
-    assert_gpu_available()
-    print("Ready to go")
-    torch.cuda.device_count()
-    # m = torch.nn.LogSoftmax(dim=-1)
-    # print(softmax(torch.Tensor([0, 0, 0, 1])))
+def load_model(vocabulary_size, file_name):
+    model = build_model(vocabulary_size, embedding_dim=256, rnn_units=1024, batch_size=1)
+    model.load_state_dict(torch.load(os.path.join(cwd, "models", file_name)))
+    model.eval()
+    return model
 
-    # model = resnet18(weights=ResNet18_Weights.DEFAULT)
-    # data = torch.rand(1, 3, 64, 64)
-    # labels = torch.rand(1, 1000)
-    # prediction = model(data)  # forward pass
-    # loss = (prediction - labels).sum()
-    # loss.backward()  # backward pass
-    # print("Done")
 
-    songs = load_training_data()
-    example_song = songs[0]
-
-    # Join our list of song strings into a single string containing all songs
+def main():
+    songs = load_songs()
     songs_joined = "\n\n".join(songs)
+    vocabulary = sorted(set(songs_joined))
+    char_to_index = {u: i for i, u in enumerate(vocabulary)}
+    index_to_char = np.array(vocabulary)
 
-    # Find all unique characters in the joined string
-    vocab = sorted(set(songs_joined))
-    print("There are", len(vocab), "unique characters in the dataset")
+    vectorized_songs = vectorize_string(songs_joined, char_to_index)
 
-    char2idx = {u: i for i, u in enumerate(vocab)}
-
-    idx2char = np.array(vocab)
-
-    # model = build_model(len(vocab), embedding_dim=256, rnn_units=1024, batch_size=1)
-    # model.load_state_dict(torch.load(os.path.join(cwd, "models", "example_model_1100.pt")))
-    # model.eval()
-    # print(generate_text(model, char2idx, idx2char, 'X'))
-
-    print('{')
-    for char, _ in zip(char2idx, range(20)):
-        print('  {:4s}: {:3d},'.format(repr(char), char2idx[char]))
-    print('  ...\n}')
-
-    vectorized_songs = vectorize_string(songs_joined, char2idx)
-
-    print(vectorized_songs)
-
-    print('{} ---- characters mapped to int ----> {}'.format(repr(songs_joined[:10]), vectorized_songs[:10]))
-
-    x_batch, y_batch = get_batch(vectorized_songs, seq_length=5, batch_size=BATCH_SIZE)
-
-    for i, (input_idx, target_idx) in enumerate(zip(np.squeeze(x_batch), np.squeeze(y_batch))):
-        print("Step {:3d}".format(i))
-        print("  input: {} ({:s})".format(input_idx, repr(idx2char[input_idx])))
-        print("  expected output: {} ({:s})".format(target_idx, repr(idx2char[target_idx])))
-
-    model = build_model(len(vocab), embedding_dim=256, rnn_units=1024, batch_size=BATCH_SIZE)
-
+    model = build_model(len(vocabulary), embedding_dim=256, rnn_units=1024, batch_size=BATCH_SIZE)
     model.summary()
 
-    loss_fn = torch.nn.CrossEntropyLoss(reduction='mean')  # TODO: reduction
+    loss_fn = torch.nn.CrossEntropyLoss(reduction='mean')
     optimizer = torch.optim.Adam(model.parameters(), lr=5e-3)
-    for i in range(1200):
+    for i in range(800):
         input_batch, target_batch = get_batch(vectorized_songs, seq_length=100, batch_size=BATCH_SIZE)
 
         prediction = model(input_batch)
-        loss = loss_fn(prediction.permute((0, 2, 1)).cpu(), torch.from_numpy(target_batch).long())  # TODO: use CUDA
+        loss = loss_fn(prediction.permute((0, 2, 1)).cpu(), torch.from_numpy(target_batch).long())
 
-        loss.backward(retain_graph=True)
+        loss.backward()
         optimizer.step()
         optimizer.zero_grad()
         if i % 10 == 0:
@@ -151,17 +105,8 @@ if __name__ == '__main__':
             torch.save(model.state_dict(), os.path.join(cwd, "models", "model_" + str(i) + ".pt"))
             print("Model has been saved")
 
-    print(generate_text(model, char2idx, idx2char, 'X'))
-    x_answer, y_answer = get_batch(vectorized_songs, seq_length=100, batch_size=BATCH_SIZE)
-    answer = model(x_answer)
-    print("Input shape:      ", x_answer.shape, " # (batch_size, sequence_length)")
-    print("Prediction shape: ", answer.shape, "# (batch_size, sequence_length, vocab_size)")
-    # softmax(answer[0])
-    # TODO: check if this is the correct replacement of tf.random.categorical
-    # TODO: check softmax documentation, there is also log_softmax and torch.nn.LogSoftmax(dim=0)
-    sampled_indices = torch.squeeze(torch.multinomial(softmax(answer[0], dim=0), 1, replacement=True)).cpu()
-    print("Prediction: ", sampled_indices)
+    print(generate_text(model, char_to_index, index_to_char, 'X'))
 
-    print("Input: \n", repr("".join(idx2char[x_answer[0]])))
-    print("Target: \n", repr("".join(idx2char[y_answer[0]])))
-    print("Next Char Predictions: \n", repr("".join(idx2char[sampled_indices])))
+
+if __name__ == '__main__':
+    main()
