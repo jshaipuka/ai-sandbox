d2501084b5f46c964a0c687918a0d3e5f7d70d77
Sergey Yaskov <yaskovdev@gmail.com>
2024-05-18 15:25:58 +0300

    - Split simple.py into simple_train.py and simple_infer.py. - Use Keras to save the model.

--- torch-sandbox/simple_infer.py
+++ torch-sandbox/simple_infer.py
@@ -1,86 +1,48 @@
 import math
 import os
 import tempfile
-import time
 
 import keras_core as keras
 import numpy as np
 import torch
 from PIL import Image
-from torch.nn.functional import softmax
 
 import mnist_loader
+from simple_common import BATCH_SIZE
 
-BATCH_SIZE = 50000
+cwd = os.path.dirname(__file__)
 
 temp_dir_name = tempfile.mkdtemp()
-
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 
 def split_into_batches(array, batch_size):
     return np.array_split(array, range(batch_size, len(array), batch_size))
 
 
-def build_model(batch_size):
-    return keras.Sequential([
-        keras.layers.Input(shape=(784,), batch_size=batch_size),
-        keras.layers.Dense(units=16),
-        keras.layers.Dense(units=16),
-        keras.layers.Dense(units=10)
-    ])
-
-
 def as_image(image):
     return Image.fromarray((255 * np.reshape(image, (math.isqrt(image.size), -1))).astype('uint8'))
-
-
-def show_digit(image):
-    as_image(image).show()
 
 
 def save_digit(file_name, image):
     as_image(image).save(os.path.join(temp_dir_name, file_name + ".png"))
 
 
-def class_with_biggest_probability(distribution):
+def pick_max_probability(distribution):
     return [np.argmax(distribution[index].cpu().detach()) for index in range(len(distribution))]
+
+
+def sample_probability_distribution(distribution):
+    answer = torch.squeeze(torch.multinomial(distribution, 1, replacement=True))
+    return answer.cpu().numpy()
 
 
 print("Ready to go")
 
-model = build_model(BATCH_SIZE)
-
-print("CUDA device count is", torch.cuda.device_count())
-print("Selected device is", device)
-model.to(device)
+model = keras.models.load_model(os.path.join(cwd, "models", "simple_model.keras"))
 
 model.summary()
 
 data = mnist_loader.load_data()
-
-training_data = data[0]
-training_inputs = training_data[0]
-training_targets = training_data[1]
-
-loss_fn = torch.nn.CrossEntropyLoss(reduction='mean')
-
-optimizer = torch.optim.Adam(model.parameters())
-
-for i in range(1000):
-    start = time.time()
-    input_batch = torch.from_numpy(np.reshape(training_inputs, [BATCH_SIZE, -1])).to(device)
-    target_batch = torch.from_numpy(training_targets).to(device)
-
-    prediction = model(input_batch)
-    loss = loss_fn(prediction, target_batch)
-
-    loss.backward()
-    optimizer.step()
-    optimizer.zero_grad()
-    end = time.time()
-    if i % 10 == 0:
-        print(i, loss.item(), end)
 
 validation_data = data[1]
 validation_inputs = validation_data[0]
@@ -94,8 +56,7 @@ for i in range(min(100, len(validation_input_batches))):
     input_batch = validation_input_batches[i]
     target_batch = validation_target_batches[i]
     prediction = model(input_batch)
-    distribution = softmax(prediction, dim=1)
-    answer_np = class_with_biggest_probability(distribution)
+    answer_np = sample_probability_distribution(prediction)
     right_answers = np.sum(answer_np == target_batch)
     for j in range(len(target_batch)):
         if target_batch[j] != answer_np[j]:
@@ -104,4 +65,4 @@ for i in range(min(100, len(validation_input_batches))):
 
 print("Saving wrongly recognized digits to ", temp_dir_name)
 for (index, expected, actual) in wrong_recognition:
-    save_digit(str(index) + "_" + str(expected) + "_" + str(actual.item()), validation_inputs[index])
+    save_digit(str(index) + "_" + str(expected) + "_" + str(actual), validation_inputs[index])
