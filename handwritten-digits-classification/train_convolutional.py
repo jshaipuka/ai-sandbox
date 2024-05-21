260141a36c7c31d9784c650b74e57bde13239fa3
Sergey Yaskov <yaskovdev@gmail.com>
2024-05-21 22:35:09 +0300

    Add a convolutional model to compare with the dense model.

--- handwritten-digits-classification/train_convolutional.py
+++ handwritten-digits-classification/train_convolutional.py
@@ -10,14 +10,19 @@ def train():
     train_images = np.expand_dims(x_train, axis=-1) / 255.0
     test_images = np.expand_dims(x_test, axis=-1) / 255.0
 
+    # Instead of providing the keras.Input layer, can build the model by passing some data through it: model.predict(train_images[[0]]).
     model = keras.Sequential([
-        keras.Input(shape=(28, 28)),
+        keras.Input(shape=(28, 28, 1)),  # Optionally can pass batch_size.
+        keras.layers.Conv2D(filters=24, kernel_size=(3, 3), activation="relu"),
+        keras.layers.MaxPool2D(pool_size=(2, 2)),
+        keras.layers.Conv2D(filters=36, kernel_size=(3, 3), activation="relu"),
+        keras.layers.MaxPool2D(pool_size=(2, 2)),
         keras.layers.Flatten(),
         keras.layers.Dense(128, activation="relu"),
         keras.layers.Dense(10, activation="softmax")
     ])
     model.summary()
-    model.compile(optimizer=keras.optimizers.SGD(learning_rate=1e-1), loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+    model.compile(optimizer=keras.optimizers.Adam(learning_rate=1e-3), loss="sparse_categorical_crossentropy", metrics=["accuracy"])
     model.fit(train_images, train_labels, batch_size=BATCH_SIZE, epochs=EPOCHS)
 
     test_loss, test_acc = model.evaluate(test_images, test_labels)
