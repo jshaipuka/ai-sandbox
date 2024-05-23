c40fef747a8410082ff82bd98e1ed3755ae46983
Sergey Yaskov <yaskovdev@gmail.com>
2024-05-24 00:11:28 +0300

    - Fix metrics (https://stackoverflow.com/a/45632689/1862286). - Cleanup.

--- facial-beauty-rating-prediction/train.py
+++ facial-beauty-rating-prediction/train.py
@@ -7,20 +7,17 @@ import numpy as np
 from common import cwd
 
 BATCH_SIZE = 64
-EPOCHS = 5
+EPOCHS = 6
 
 
 def train():
     archive_path = sys.argv[1]
     print("Going to read data from", archive_path)
     b = np.load(archive_path)
-    np.asarray(b["y"], dtype=float)
-    x, labels = b["X"], np.asarray(b["y"], dtype=float)
-    train_length = 4500
-    x_train, train_labels = x[:train_length], labels[:train_length]
-    x_test, test_labels = x[train_length:], labels[train_length:]
-    train_images = x_train / 255.0
-    test_images = x_test / 255.0
+    x, labels = b["X"] / 255.0, np.asarray(b["y"], dtype=float)
+    train_length = 4900
+    train_images, train_labels = x[:train_length], labels[:train_length]
+    test_images, test_labels = x[train_length:], labels[train_length:]
 
     model = keras.Sequential([
         keras.Input(shape=(350, 350, 3)),
@@ -28,17 +25,17 @@ def train():
         keras.layers.MaxPool2D(pool_size=(2, 2)),
         keras.layers.Conv2D(filters=64, kernel_size=(3, 3), activation="relu"),
         keras.layers.MaxPool2D(pool_size=(2, 2)),
-        keras.layers.Conv2D(64, (3, 3), activation='relu'),
+        keras.layers.Conv2D(64, (3, 3), activation="relu"),
         keras.layers.Flatten(),
-        keras.layers.Dense(64, activation='relu'),
-        keras.layers.Dense(1, activation='linear')
+        keras.layers.Dense(64, activation="relu"),
+        keras.layers.Dense(1, activation="linear")
     ])
     model.summary()
-    model.compile(optimizer=keras.optimizers.Adam(learning_rate=1e-3), loss="mean_squared_error", metrics=["accuracy"])
+    model.compile(optimizer=keras.optimizers.Adam(learning_rate=1e-3), loss="mean_squared_error", metrics=["mae", "mape", "cosine_similarity"])
     model.fit(train_images, train_labels, batch_size=BATCH_SIZE, epochs=EPOCHS)
 
-    test_loss, test_acc = model.evaluate(test_images, test_labels)
-    print("Accuracy:", test_acc)
+    loss_and_metrics = model.evaluate(test_images, test_labels, return_dict=True)
+    print("Loss and metrics of the model:", loss_and_metrics)
 
     file_name = "model.keras"
     model.save(os.path.join(cwd, file_name))
