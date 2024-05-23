d1371b2c18af38d7493582d6c90dfbf498ef6c23
Sergey Yaskov <yaskovdev@gmail.com>
2024-05-23 23:12:07 +0300

    Draft implementation.

--- facial-beauty-rating-prediction/train.py
+++ facial-beauty-rating-prediction/train.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 import keras_core as keras
 import numpy as np
@@ -10,30 +11,36 @@ EPOCHS = 5
 
 
 def train():
-    (x_train, train_labels), (x_test, test_labels) = keras.datasets.mnist.load_data()
-    train_images = np.expand_dims(x_train, axis=-1) / 255.0
-    test_images = np.expand_dims(x_test, axis=-1) / 255.0
+    archive_path = sys.argv[1]
+    print("Going to read data from", archive_path)
+    b = np.load(archive_path)
+    np.asarray(b["y"], dtype=float)
+    x, labels = b["X"], np.asarray(b["y"], dtype=float)
+    train_length = 4500
+    x_train, train_labels = x[:train_length], labels[:train_length]
+    x_test, test_labels = x[train_length:], labels[train_length:]
+    train_images = x_train / 255.0
+    test_images = x_test / 255.0
 
-    # Instead of providing the keras.Input layer, can build the model by passing some data through it: model.predict(train_images[[0]]).
-    # Or you can call model.build((None, 28, 28, 1)).
     model = keras.Sequential([
-        keras.Input(shape=(28, 28, 1)),  # Optionally can pass batch_size.
-        keras.layers.Conv2D(filters=24, kernel_size=(3, 3), activation="relu"),
+        keras.Input(shape=(350, 350, 3)),
+        keras.layers.Conv2D(filters=32, kernel_size=(3, 3), activation="relu"),
         keras.layers.MaxPool2D(pool_size=(2, 2)),
-        keras.layers.Conv2D(filters=36, kernel_size=(3, 3), activation="relu"),
+        keras.layers.Conv2D(filters=64, kernel_size=(3, 3), activation="relu"),
         keras.layers.MaxPool2D(pool_size=(2, 2)),
+        keras.layers.Conv2D(64, (3, 3), activation='relu'),
         keras.layers.Flatten(),
-        keras.layers.Dense(128, activation="relu"),
-        keras.layers.Dense(10, activation="softmax")
+        keras.layers.Dense(64, activation='relu'),
+        keras.layers.Dense(1, activation='linear')
     ])
     model.summary()
-    model.compile(optimizer=keras.optimizers.Adam(learning_rate=1e-3), loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+    model.compile(optimizer=keras.optimizers.Adam(learning_rate=1e-3), loss="mean_squared_error", metrics=["accuracy"])
     model.fit(train_images, train_labels, batch_size=BATCH_SIZE, epochs=EPOCHS)
 
     test_loss, test_acc = model.evaluate(test_images, test_labels)
     print("Accuracy:", test_acc)
 
-    file_name = "model_convolutional.keras"
+    file_name = "model.keras"
     model.save(os.path.join(cwd, file_name))
 
 
