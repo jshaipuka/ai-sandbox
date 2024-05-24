a7d5ce26295572f22cd6a59cc94c994e6fb461cf
Sergey Yaskov <yaskovdev@gmail.com>
2024-05-24 12:15:44 +0300

    Plug validation_data to the fitting process.

--- facial-beauty-rating-prediction/train.py
+++ facial-beauty-rating-prediction/train.py
@@ -7,7 +7,7 @@ import numpy as np
 from common import cwd
 
 BATCH_SIZE = 64
-EPOCHS = 50
+EPOCHS = 6
 
 
 def train():
@@ -16,8 +16,10 @@ def train():
     b = np.load(archive_path)
     x, labels = b["X"] / 255.0, np.asarray(b["y"], dtype=float)
     train_length = 4900
-    train_images, train_labels = x[:train_length], labels[:train_length]
-    test_images, test_labels = x[train_length:], labels[train_length:]
+    train_indices = np.random.choice(len(x), train_length, replace=False)
+    test_indices = np.delete(np.arange(len(x)), train_indices)
+    train_images, train_labels = np.take(x, train_indices, axis=0), np.take(labels, train_indices, axis=0)
+    test_images, test_labels = np.take(x, test_indices, axis=0), np.take(labels, test_indices, axis=0)
 
     model = keras.Sequential([
         keras.Input(shape=(350, 350, 3)),
@@ -32,7 +34,7 @@ def train():
     ])
     model.summary()
     model.compile(optimizer=keras.optimizers.Adam(learning_rate=1e-3), loss="mse", metrics=["mae", "mape", "cosine_similarity"])
-    model.fit(train_images, train_labels, batch_size=BATCH_SIZE, epochs=EPOCHS)
+    model.fit(train_images, train_labels, batch_size=BATCH_SIZE, epochs=EPOCHS, validation_data=(test_images, test_labels))
 
     loss_and_metrics = model.evaluate(test_images, test_labels, return_dict=True)
     print("Loss and metrics of the model:", loss_and_metrics)
