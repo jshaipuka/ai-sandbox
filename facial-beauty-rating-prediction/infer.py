0abe68edb411f03a8ebf8b89a2d258b3592b766b
Sergey Yaskov <yaskovdev@gmail.com>
2024-05-24 14:39:46 +0300

    During the inference, try using part of the data loaded from the archive to test the model.

--- facial-beauty-rating-prediction/infer.py
+++ facial-beauty-rating-prediction/infer.py
@@ -10,17 +10,24 @@ from PIL import Image
 from common import cwd
 
 
+def _extract_expected_score(f):
+    name = f.split(".")[0]
+    tokens = name.split("_")
+    return float(tokens[1] + "." + tokens[2])
+
+
 def infer():
-    # TODO: try using part of the data loaded from the archive to test the model
     image_folder = sys.argv[1]
     print("Going to read images from", image_folder)
-    paths = [os.path.join(image_folder, f) for f in listdir(image_folder)]
-    files = [p for p in paths if isfile(os.path.join(image_folder, p))]
-    images = np.array([np.array(Image.open(f).convert("RGB")) / 255.0 for f in files])
+    files = [f for f in listdir(image_folder) if isfile(os.path.join(image_folder, f))]
+    expected_scores = [_extract_expected_score(f) for f in files]
+    full_paths = [os.path.join(image_folder, p) for p in files]
+    images = np.array([np.array(Image.open(f).convert("RGB")) / 255.0 for f in full_paths])
     model = keras.models.load_model(os.path.join(cwd, "model.keras"))
     model.summary()
-    predictions = model.predict(np.array(images))
-    print(list(zip(paths, np.squeeze(predictions))))
+    predicted_scores = model.predict(np.array(images))
+    for i, predicted_score in enumerate(np.squeeze(predicted_scores)):
+        print("For image", files[i], "predicted score is", predicted_score, ", expected score is", expected_scores[i])
 
 
 if __name__ == "__main__":
--- facial-beauty-rating-prediction/train.py
+++ facial-beauty-rating-prediction/train.py
@@ -3,6 +3,7 @@ import sys
 
 import keras_core as keras
 import numpy as np
+from PIL import Image
 
 from common import cwd
 
@@ -10,16 +11,30 @@ BATCH_SIZE = 64
 EPOCHS = 6
 
 
+def _training_validation_test_indices(training_length, validation_length, test_length):
+    shuffled_indices = np.arange(training_length + validation_length + test_length)
+    np.random.shuffle(shuffled_indices)
+    [training_indices, validation_indices, test_indices] = np.split(shuffled_indices, [training_length, training_length + validation_length])
+    return training_indices, validation_indices, test_indices
+
+
 def train():
     archive_path = sys.argv[1]
     print("Going to read data from", archive_path)
     data = np.load(archive_path)
     images, scores = data["X"] / 255.0, np.asarray(data["y"], dtype=float)
-    training_length = 4900
-    training_indices = np.random.choice(len(images), training_length, replace=False)
-    validation_indices = np.delete(np.arange(len(images)), training_indices)
+    training_indices, validation_indices, test_indices = _training_validation_test_indices(4900, 500, 100)
+    assert len(images) == len(scores) == len(training_indices) + len(validation_indices) + len(test_indices), "Samples, labels or split length mismatch"
     training_images, training_scores = np.take(images, training_indices, axis=0), np.take(scores, training_indices, axis=0)
     validation_images, validation_scores = np.take(images, validation_indices, axis=0), np.take(scores, validation_indices, axis=0)
+    test_images, test_scores = np.take(images, test_indices, axis=0), np.take(scores, test_indices, axis=0)
+
+    test_data_folder = os.path.join(os.path.dirname(archive_path), "test")
+    print("Going to save test data to", test_data_folder)
+    for i, image_bytes in enumerate(test_images):
+        score = test_scores[i]
+        image = Image.fromarray(np.asarray(image_bytes * 255).astype(np.uint8), "RGB")
+        image.save(os.path.join(test_data_folder, "{:04d}".format(i) + "_" + str(score).replace(".", "_") + ".png"))
 
     model = keras.Sequential([
         keras.Input(shape=(350, 350, 3)),
