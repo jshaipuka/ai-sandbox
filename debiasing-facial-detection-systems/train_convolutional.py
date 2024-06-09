d1cdabcfd6f9d07c97b786ed323e861315ba5072
Sergey Yaskov <yaskovdev@gmail.com>
2024-06-09 15:01:57 +0300

    Create new project.

---
+++ debiasing-facial-detection-systems/train_convolutional.py
@@ -0,0 +1,43 @@
+import os
+
+import comet_ml as comet
+import keras_core as keras
+import numpy as np
+
+from common import cwd
+
+BATCH_SIZE = 64
+EPOCHS = 5
+
+
+def train():
+    experiment = comet.Experiment(project_name="handwritten-digits-classification")
+    (x_train, train_labels), (x_test, test_labels) = keras.datasets.mnist.load_data()
+    train_images = np.expand_dims(x_train, axis=-1) / 255.0
+    test_images = np.expand_dims(x_test, axis=-1) / 255.0
+
+    # Instead of providing the keras.Input layer, can build the model by passing some data through it: model.predict(train_images[[0]]).
+    # Or you can call model.build((None, 28, 28, 1)).
+    model = keras.Sequential([
+        keras.Input(shape=(28, 28, 1)),  # Optionally can pass batch_size.
+        keras.layers.Conv2D(filters=24, kernel_size=(3, 3), activation="relu"),
+        keras.layers.MaxPool2D(pool_size=(2, 2)),
+        keras.layers.Conv2D(filters=36, kernel_size=(3, 3), activation="relu"),
+        keras.layers.MaxPool2D(pool_size=(2, 2)),
+        keras.layers.Flatten(),
+        keras.layers.Dense(128, activation="relu"),
+        keras.layers.Dense(10, activation="softmax")
+    ])
+    model.summary()
+    model.compile(optimizer=keras.optimizers.Adam(learning_rate=1e-3), loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+    model.fit(train_images, train_labels, batch_size=BATCH_SIZE, epochs=EPOCHS)
+
+    test_loss, test_acc = model.evaluate(test_images, test_labels)
+    print("Accuracy:", test_acc)
+
+    file_name = "model_convolutional.keras"
+    model.save(os.path.join(cwd, file_name))
+
+
+if __name__ == "__main__":
+    train()
--- torch-sandbox/experiments.py
+++ torch-sandbox/experiments.py
@@ -1,27 +1,43 @@
 import torch
-from torch import nn
 
-batch_size = 3
-seq_length = 5
-batch = torch.tensor([i for i in range(batch_size * seq_length)]).view((batch_size, seq_length))
-print(batch)
-for step in range(batch.shape[1] - 1):
-    x, y = batch[:, step], batch[:, step + 1]
-    print(x, y)
 
-xs = torch.tensor([
-    [[0, 1], [0, 2], [0, 3]],
-    [[0, 4], [0, 5], [0, 6]],
-    [[0, 7], [0, 8], [0, 9]],
-    [[0, 10], [0, 11], [0, 12]]
-])
-print(xs)
-print(xs.shape)
+def exp_0():
+    batch_size = 3
+    seq_length = 5
+    batch = torch.tensor([i for i in range(batch_size * seq_length)]).view((batch_size, seq_length))
+    print(batch)
+    for step in range(batch.shape[1] - 1):
+        x, y = batch[:, step], batch[:, step + 1]
+        print(x, y)
 
-reshaped_xs = xs.view(12, -1)
-print(reshaped_xs)
-print(reshaped_xs.shape)
+    xs = torch.tensor([
+        [[0, 1], [0, 2], [0, 3]],
+        [[0, 4], [0, 5], [0, 6]],
+        [[0, 7], [0, 8], [0, 9]],
+        [[0, 10], [0, 11], [0, 12]]
+    ])
+    print(xs)
+    print(xs.shape)
 
-embedding = nn.Embedding(10, 3)
-input = torch.LongTensor([[1, 2, 4, 5], [4, 3, 2, 9]])
-print(embedding(input))
+    reshaped_xs = xs.view(12, -1)
+    print(reshaped_xs)
+    print(reshaped_xs.shape)
+
+    embedding = nn.Embedding(10, 3)
+    input = torch.LongTensor([[1, 2, 4, 5], [4, 3, 2, 9]])
+    print(embedding(input))
+
+
+# Karpathy's lecture about transformers
+def exp_1():
+    b, t, c = 4, 8, 32
+    head_size = 16
+    value = torch.nn.Linear(c, head_size, bias=False)
+    x = torch.randn(b, t, c)
+    print(x.shape)  # (b, t, c)
+    print(value.weight.shape)  # (head_size, c)
+    print(value(x).shape)  # (b, t, c) x (b, c, head_size) -> (b, t, head_size)
+
+
+if __name__ == "__main__":
+    exp_1()
