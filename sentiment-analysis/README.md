8b9a6e559b58efe4844e1e9f29f09b647e91b7be
Sergey Yaskov <yaskovdev@gmail.com>
2025-06-28 20:59:12 +0300

    Switch to Jupiter notebook to not run the whole code from the beginning every time.

--- sentiment-analysis/README.md
+++ sentiment-analysis/README.md
@@ -1,5 +1,18 @@
 https://docs.pytorch.org/tutorials/beginner/onnx/export_simple_model_to_onnx_tutorial.html
 
+```shell
+conda create -n sentiment-analysis python=3.12
+conda activate sentiment-analysis
+conda install pytorch torchvision torchaudio -c pytorch-nightly
+
+# https://spacy.io/usage
+conda install -c conda-forge spacy
+python -m spacy download en_core_web_sm
+
+conda install -c huggingface -c conda-forge datasets
+conda install conda-forge::transformers
+```
+
 ```python
 import onnx  # then install
 ```
--- sentiment-analysis/main.py
+++ sentiment-analysis/main.py
@@ -1,5 +1,6 @@
+import spacy
 import torch
-from torch import nn
+from datasets import load_dataset, Dataset
 
 
 def export_model(model, tensor):
@@ -7,21 +8,35 @@ def export_model(model, tensor):
 
 
 def main():
-    batch = torch.tensor([
-        [1, 2, 3],
-        [1, 2, 3],
-        [1, 2, 3],
-        [1, 2, 3],
-        [1, 2, 3]
-    ], dtype=torch.float32)
-    model = nn.Sequential(
-        nn.Linear(3, 2),
-        nn.ReLU()
-    )
-    print(batch.shape)
-    print(model)
-    print(model(batch))
-    export_model(model, batch)
+    nlp = spacy.load('en_core_web_sm')
+    dataset = load_dataset('Sp1786/multiclass-sentiment-analysis-dataset')
+    train_dataset: Dataset = dataset['train']
+    # limit = 10
+    # print(train_dataset['text'][:limit])
+    # print(train_dataset['label'][:limit])
+    # print(train_dataset['sentiment'][:limit])
+
+    ignore = {'SPACE', 'PUNCT', 'PART'}
+    text: str = ' '.join(train_dataset['text'])
+    nlp.max_length = len(text)
+    doc = nlp(text)
+    words: set[str] = {token.text.lower() for token in doc if token.pos_ not in ignore}
+    print(len(words))
+    # batch = torch.tensor([
+    #     [1, 2, 3],
+    #     [1, 2, 3],
+    #     [1, 2, 3],
+    #     [1, 2, 3],
+    #     [1, 2, 3]
+    # ], dtype=torch.float32)
+    # model = nn.Sequential(
+    #     nn.Linear(3, 2),
+    #     nn.ReLU()
+    # )
+    # print(batch.shape)
+    # print(model)
+    # print(model(batch))
+    # export_model(model, batch)
 
 
 if __name__ == '__main__':
