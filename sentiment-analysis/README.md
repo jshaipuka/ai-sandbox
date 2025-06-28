cb9a8c99d25b5f2a23e4da231742d38c12a61c69
Sergey Yaskov <yaskovdev@gmail.com>
2025-06-28 11:43:54 +0300

    Try Neutron visualizer.

---
+++ sentiment-analysis/README.md
@@ -0,0 +1,9 @@
+https://docs.pytorch.org/tutorials/beginner/onnx/export_simple_model_to_onnx_tutorial.html
+
+```python
+import onnx  # then install
+```
+
+```shell
+pip install netron && netron model.onnx
+```
--- sentiment-analysis/main.py
+++ sentiment-analysis/main.py
@@ -1,24 +1,21 @@
-import hiddenlayer as hl
 import torch
-from torch.utils.tensorboard import SummaryWriter
+from torch import nn
 
 
-def visualize_model(model, tensor):
-    writer = SummaryWriter('runs/visualization')
-    writer.add_graph(model, tensor)
-    writer.close()
-    print('tensorboard --logdir=runs/visualization')
+def export_model(model, tensor):
+    torch.onnx.export(model, tensor, "model.onnx")
+
 
 def main():
     tensor = torch.tensor([1, 2, 3], dtype=torch.float32)
-    model = torch.nn.Sequential(
-        torch.nn.Linear(3, 2),
-        torch.nn.ReLU()
+    model = nn.Sequential(
+        nn.Linear(3, 2),
+        nn.ReLU()
     )
     print(tensor.shape)
     print(model)
     print(model(tensor))
-    visualize_model(model, tensor)
+    export_model(model, tensor)
 
 
 if __name__ == '__main__':
